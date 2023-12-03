3f8f29ebf78cf50bfc42b3b6939b5ee350f9ab8b
Juan Nicolas Mendoza Roncancio <119080673+JuanNicolasMendozaRoncancio@users.noreply.github.com>
2023-12-02 19:52:43 -0500

    Se actualiza el modelo LDA

--- scripts/training/main.py
+++ scripts/training/main.py
@@ -5,13 +5,12 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics.pairwise import cosine_similarity
 
 param_grid = {
-    'n_components': [5, 10, 15, 20],  # Número de tópicos
+    'n_components': [5,10,15,20],  # Número de tópicos
     'learning_method': ['batch', 'online'],  # Método de aprendizaje
-    'max_iter': [10, 50, 100],  # Número máximo de iteraciones
+    'max_iter': [10,50,100],  # Número máximo de iteraciones
 }
 lda = LatentDirichletAllocation()
 grid_search = GridSearchCV(lda, param_grid=param_grid, cv=5, verbose=2)
-
 
 dfBoW = pd.read_excel("/content/Mlds6_Proyecto_Aplicado/docs/data/BoWrepre.xlsx")
 X_np = dfBoW.to_numpy()
@@ -19,12 +18,17 @@ X_np = dfBoW.to_numpy()
 
 grid_search.fit(X_np)
 
+best_lda = grid_search.best_estimator_
+components = best_lda.components_
+
 lda = LatentDirichletAllocation(
     max_iter= grid_search.best_params_["max_iter"],
     n_components=grid_search.best_params_["n_components"],
     learning_method= grid_search.best_params_['learning_method']
     ).fit(X_np)
 
+df = pd.read_excel("/content/Mlds6_Proyecto_Aplicado/docs/data/VocabBow.xlsx")
+vocab = df.to_numpy()
 
 # Iteramos sobre cada tópico
 for i, comp in enumerate(components):
@@ -36,11 +40,13 @@ for i, comp in enumerate(components):
             key=lambda x: x[1],
             reverse=True
             )[:15]
+    # Convertimos los términos a cadenas de texto antes de unirlos
+    terms_str = list(map(lambda x: str(x[0]), sorted_terms))
     # Mostramos los términos más importantes en cada tópico
     print(
             "Tópico {}: {}".format(
                 i,
-                " ".join(list(map(lambda x:x[0], sorted_terms)))
+                " ".join(terms_str)
                 )
             )
 
